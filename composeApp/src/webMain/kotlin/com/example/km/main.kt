5945cf9520ec402d63bcae9c28c13e3fde94959c
jab0n <jab0n@outlook.com>
2026-05-07 05:24:38 -0600

    Commit inicial

---
+++ composeApp/src/webMain/kotlin/com/example/km/main.kt
@@ -0,0 +1,11 @@
+package com.example.km
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
