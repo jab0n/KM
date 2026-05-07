64973c47289b5723fe155664c6c5ad9662b84b84
jab0n <jab0n@outlook.com>
2026-05-07 05:37:23 -0600

    mejoras en el aspecto visual

--- composeApp/src/commonMain/kotlin/com/example/km/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/km/App.kt
@@ -4,17 +4,25 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 
 import km.composeapp.generated.resources.Res
@@ -25,26 +33,72 @@ import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Clock
 
+data class Country(val name: String, val zone: TimeZone)
+
+fun currentTimeAt(location: String, zone: TimeZone): String {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
+
+    val time = Clock.System.now()
+    val localTime = time.toLocalDateTime(zone).time
+
+    return "The time in $location is ${localTime.formatted()}"
+}
+
+fun countries() = listOf(
+    Country("Japan", TimeZone.of("Asia/Tokyo")),
+    Country("France", TimeZone.of("Europe/Paris")),
+    Country("Mexico", TimeZone.of("America/Mexico_City")),
+    Country("Indonesia", TimeZone.of("Asia/Jakarta")),
+    Country("Egypt", TimeZone.of("Africa/Cairo")),
+)
+
 @Composable
 @Preview
-fun App() {
+fun App(countries: List<Country> = countries()) {
     MaterialTheme {
-        var location by remember { mutableStateOf("Europe/Paris") }
+        var showCountries by remember { mutableStateOf(false) }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
 
         Column(
             modifier = Modifier
+                .padding(20.dp)
                 .safeContentPadding()
-                .fillMaxSize()
+                .fillMaxSize(),
         ) {
-            Text(timeAtLocation)
-            TextField(value = location, onValueChange = { location = it })
-            Button(onClick = { timeAtLocation = currentTimeAt(location) ?: "Invalid Location" }) {
-                Text("Show Time At Location")
+            Text(
+                timeAtLocation,
+                style = TextStyle(fontSize = 20.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
+            )
+            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
+                DropdownMenu(
+                    expanded = showCountries,
+                    onDismissRequest = { showCountries = false }
+                ) {
+                    countries().forEach { (name, zone) ->
+                        DropdownMenuItem(
+                            text = {   Text(name)},
+                            onClick = {
+                                timeAtLocation = currentTimeAt(name, zone)
+                                showCountries = false
+                            }
+                        )
+                    }
+                }
+            }
+
+            Button(modifier = Modifier.padding(start = 20.dp, top = 10.dp),
+                onClick = { showCountries = !showCountries }) {
+                Text("Select Location")
             }
         }
     }
 }
+
+
+
+
 
 fun currentTimeAt(location: String): String? {
     fun LocalTime.formatted() = "$hour:$minute:$second"
